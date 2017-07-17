540a4c33e66f8508a016dab2dbe1dfef64b50610
Sebastian Hartmann <sebastian.hartmann@simovative.com>
2017-07-17 12:57:21 +0200

    Made UrlMatcher check Uri by default  * Added $matchPath bool param to UrlMatcherInterface  * Replaced matching with a simple regex  * removed url components and instance variables

--- src/Http/Url/UrlMatcher.php
+++ src/Http/Url/UrlMatcher.php
@@ -3,18 +3,9 @@ namespace Simovative\Zeus\Http\Url;
 
 /**
  * @author mnoerenberg
+ * @author shartmann
  */
 class UrlMatcher implements UrlMatcherInterface {
-	
-	/**
-	 * @var Url
-	 */
-	private $url;
-
-	/**
-	 * @var string[]
-	 */
-	private $components;
 	
 	/**
 	 * @var string
@@ -26,133 +17,45 @@ class UrlMatcher implements UrlMatcherInterface {
 	 * @param string $urlPrefix
 	 */
 	public function __construct($urlPrefix = '') {
-		$this->urlPrefix = $urlPrefix;
+		$this->urlPrefix = trim($urlPrefix, '/');
 	}
 	
 	/**
 	 * @inheritdoc
-	 * @author mnoerenberg
+	 * @author shartmann
 	 */
-	public function getComponent($position) {
-		if ($this->hasComponent($position)) {
-			$components = $this->getComponents();
-			return $components[$position];
+	public function match(Url $url, $route, $matchPath = true) {
+		if($matchPath) {
+			$toMatch = '/' . ltrim($this->urlPrefix . '/' . $url->getPath(), '/');
+		} else {
+			$toMatch = strtok('/', (string) $url) . '/' . $this->urlPrefix . '/' . $url->getPath();
 		}
-			
-		throw new \LogicException('position does not exist.');
-	}
-	
-	/**
-	 * @inheritdoc
-	 * @author Benedikt Schaller
-	 */
-	public function hasComponent($position) {
-		if (array_key_exists($position, $this->getComponents())) {
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Returns the components of the url.
-	 *
-	 * @author Benedikt Schaller
-	 * @return string[]|array
-	 */
-	private function getComponents() {
-		if ($this->components === null) {
-			$this->components = explode('/', $this->url->getPath());
-		}
-		return $this->components;
-	}
-	
-	/**
-	 * Returns true if size of route components is equal to url components.
-	 * 
-	 * @author mnoerenberg
-	 * @param string[] $routeComponents
-	 * @return boolean
-	 */
-	private function hasEqualSize($routeComponents) {
-		return count($routeComponents) == count($this->getComponents());
-	}
-	
-	/**
-	 * Returns true if route is equal to url.
-	 * 
-	 * @author mnoerenberg
-	 * @param string $route
-	 * @return boolean
-	 */
-	private function isEqual($route) {
-		return $this->urlPrefix . $route == $this->url->getPath();
-	}
-
-	/**
-	 * Change the url to match.
-	 *
-	 * @author Benedikt Schaller
-	 * @param $url
-	 * @return void
-	 */
-	private function changeUrl($url) {
-		if ($url === $this->url) {
-			return;
-		}
-		$this->url = $url;
-		$this->components = null;
-	}
-	
-	/**
-	 * @inheritdoc
-	 * @author mnoerenberg
-	 */
-	public function match(Url $url, $route) {
-		$this->changeUrl($url);
-
-		// return false if route is static and is not equal.
+		
+		// simple match
 		if (strpos($route, '%') === false) {
-			return $this->isEqual($route);
+			return $toMatch === $route;
 		}
 		
-		// check size
-		$routeComponents = explode('/', $route);
-		if (! $this->hasEqualSize($routeComponents)) {
-			return false;
-		}
-		
-// 		$diff = array_diff($this->components, $routeComponents);
-		
-		// for each component
-		foreach ($routeComponents as $index => $routeComponent) {
-			$urlComponent = $this->components[$index];
-			
-			if ($routeComponent == '%d') {
-				if (! is_numeric($urlComponent)) {
-					return false;
-				}
-				continue;
-			}
-			
-			if ($routeComponent == '%s') {
-				continue;
-			}
-			
-			if ($routeComponent != $urlComponent) {
-				return false;
-			}
-		}
-		
-		return true;
+		/*
+		 * If the URL/URI contains variables we need to create a suitable pattern first
+		 * Currently only %d for integer values and %s for ANY KIND OF strings are supported (Yes, even the weird ones).
+		 *
+		 * Whatever variable you choose, keep in mind that '/' will ALWAYS terminate your match, meaning that you can't
+		 * have any strings containing '/' AS PART OF %s in your routing table.     --sh
+		 */
+		$search = array('~', '/%s', '/%d');
+		$replace = array('\\~', '/([^/]+)', '/([0-9]+)');
+		$pattern = '~^' . str_replace($search, $replace, $route) . '/?(\?.*)?$~i';
+		return 1 === preg_match($pattern, $toMatch);
 	}
 	
 	/**
 	 * @author Benedikt Schaller
 	 * @inheritdoc
 	 */
-	public function matchAny(Url $url, $routes) {
+	public function matchAny(Url $url, $routes, $matchPath = true) {
 		foreach ($routes as $route) {
-			if ($this->match($url, $route)) {
+			if ($this->match($url, $route, $matchPath)) {
 				return true;
 			}
 		}
--- src/Http/Url/UrlMatcherInterface.php
+++ src/Http/Url/UrlMatcherInterface.php
@@ -5,43 +5,27 @@ namespace Simovative\Zeus\Http\Url;
  * @author mnoerenberg
  */
 interface UrlMatcherInterface {
-	
-	/**
-	 * Returns a component by position.
-	 *
-	 * @author mnoerenberg
-	 * @param int $position
-	 * @throws \LogicException
-	 * @return string
-	 */
-	public function getComponent($position);
-	
-	/**
-	 * Returns true if position exists.
-	 *
-	 * @author mnoerenberg
-	 * @param int $position
-	 * @return boolean
-	 */
-	public function hasComponent($position);
-	
 	/**
 	 * Returns true if route is matched.
 	 *
 	 * @author mnoerenberg
+	 * @author shartmann
 	 * @param Url $url
 	 * @param string $route
+	 * @param bool $matchPath if set to true the server name is ignored
 	 * @return bool
 	 */
-	public function match(Url $url, $route);
+	public function match(Url $url, $route, $matchPath);
 	
 	/**
 	 * Returns true if any of the given routes is matched.
 	 *
 	 * @author Benedikt Schaller
+	 * @author shartmann
 	 * @param Url $url
 	 * @param string[] $routes
+	 * @param bool $matchPath if set to true the server name is ignored
 	 * @return bool
 	 */
-	public function matchAny(Url $url, $routes);
+	public function matchAny(Url $url, $routes, $matchPath = true);
 }
