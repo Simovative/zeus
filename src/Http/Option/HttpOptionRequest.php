f01e2b946a3a1914bb20ed61dd377dcfa1590f33
benedikt.schaller <benedikt.schaller@simovative.com>
2022-09-12 14:42:49 +0200

    ACFIVE-38341
    - CR

--- src/Http/Option/HttpOptionRequest.php
+++ src/Http/Option/HttpOptionRequest.php
@@ -3,14 +3,10 @@ namespace Simovative\Zeus\Http\Get;
 
 use Simovative\Zeus\Http\Request\HttpRequest;
 
-/**
- * @author mnoerenberg
- */
 class HttpOptionRequest extends HttpRequest {
 	
 	/**
 	 * @inheritdoc
-	 * @author mnoerenberg
 	 */
 	public function isOption(): bool {
 		return true;
